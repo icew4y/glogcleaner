a285ac7ac3a131399297946237ac0f4b6c2bfb11
DESKTOP-8A59JRS\weilai <weilaizhibentm@gmail.com>
2019-07-04 23:12:13 +0800

    linux日志定时清理。

--- glogcleaner.go
+++ glogcleaner.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/xuri/glc"
+	"sync"
 	"time"
 )
 
@@ -17,14 +18,14 @@ func main() {
 	//	}
 	//	fmt.Print("yes")
 	//}
-	var cstZone = time.FixedZone("CST", 8*3600)
+	var workGroup sync.WaitGroup
+	workGroup.Add(1)
 
-	fmt.Print(time.Now().In(cstZone).Format("2006-01-02 15:04:05") + "\n")
 	glc.NewGLC(glc.InitOption{
 		Path:     "/root/projects/vivo_module/bin/x64/Release/logs/",
 		Prefix:   "backend",
-		Interval: time.Duration(time.Hour * 12),
+		Interval: time.Duration(time.Second * 30),
 		Reserve:  time.Duration(time.Hour * 12),
 	})
-	defer func() { select {} }()
+	workGroup.Wait()
 }
